22d9df8cc31625ec1486f50d5c9c70e1af98b0bd
Beth Sebian <esebian@gmail.com>
2016-02-17 14:26:00 -0700

    Install GitHub authorization

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'dashboard', to: "users#index"
   get '/jane-doe', to: "representatives#show"
+  get '/auth/:provider/callback', to: 'sessions#create'
 end
